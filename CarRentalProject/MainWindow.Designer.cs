c7180b280323cf6e6bbd34543a28c10596832a43
Navya Balan <pnavyabalan@gmail.com>
2024-02-05 14:45:14 +0530

    first commit

--- CarRentalProject/MainWindow.Designer.cs
+++ CarRentalProject/MainWindow.Designer.cs
@@ -34,7 +34,6 @@ namespace CarRentalProject
             this.manageRentedRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addRentedRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editbRentedRecordsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.manageUsersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.tsiLogintxt = new System.Windows.Forms.ToolStripStatusLabel();
@@ -51,14 +50,14 @@ namespace CarRentalProject
             this.manageUsersToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1495, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(1495, 28);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // manageVechicleListingToolStripMenuItem
             // 
             this.manageVechicleListingToolStripMenuItem.Name = "manageVechicleListingToolStripMenuItem";
-            this.manageVechicleListingToolStripMenuItem.Size = new System.Drawing.Size(182, 26);
+            this.manageVechicleListingToolStripMenuItem.Size = new System.Drawing.Size(182, 24);
             this.manageVechicleListingToolStripMenuItem.Text = "Manage Vechicle Listing";
             this.manageVechicleListingToolStripMenuItem.Click += new System.EventHandler(this.manageVechicleListingToolStripMenuItem_Click);
             // 
@@ -66,36 +65,29 @@ namespace CarRentalProject
             // 
             this.manageRentedRecordsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addRentedRecordsToolStripMenuItem,
-            this.viewRecordsToolStripMenuItem,
-            this.editbRentedRecordsToolStripMenuItem});
+            this.viewRecordsToolStripMenuItem});
             this.manageRentedRecordsToolStripMenuItem.Name = "manageRentedRecordsToolStripMenuItem";
-            this.manageRentedRecordsToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.manageRentedRecordsToolStripMenuItem.Size = new System.Drawing.Size(185, 24);
             this.manageRentedRecordsToolStripMenuItem.Text = "Manage Rented Records";
             // 
             // addRentedRecordsToolStripMenuItem
             // 
             this.addRentedRecordsToolStripMenuItem.Name = "addRentedRecordsToolStripMenuItem";
-            this.addRentedRecordsToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
+            this.addRentedRecordsToolStripMenuItem.Size = new System.Drawing.Size(228, 26);
             this.addRentedRecordsToolStripMenuItem.Text = "Add Rented Records";
             this.addRentedRecordsToolStripMenuItem.Click += new System.EventHandler(this.addRentedRecordsToolStripMenuItem_Click);
             // 
             // viewRecordsToolStripMenuItem
             // 
             this.viewRecordsToolStripMenuItem.Name = "viewRecordsToolStripMenuItem";
-            this.viewRecordsToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
+            this.viewRecordsToolStripMenuItem.Size = new System.Drawing.Size(228, 26);
             this.viewRecordsToolStripMenuItem.Text = "View Records";
             this.viewRecordsToolStripMenuItem.Click += new System.EventHandler(this.viewRecordsToolStripMenuItem_Click);
-            // 
-            // editbRentedRecordsToolStripMenuItem
-            // 
-            this.editbRentedRecordsToolStripMenuItem.Name = "editbRentedRecordsToolStripMenuItem";
-            this.editbRentedRecordsToolStripMenuItem.Size = new System.Drawing.Size(235, 26);
-            this.editbRentedRecordsToolStripMenuItem.Text = "Editb Rented Records";
             // 
             // manageUsersToolStripMenuItem
             // 
             this.manageUsersToolStripMenuItem.Name = "manageUsersToolStripMenuItem";
-            this.manageUsersToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.manageUsersToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
             this.manageUsersToolStripMenuItem.Text = "Manage Users";
             this.manageUsersToolStripMenuItem.Click += new System.EventHandler(this.manageUsersToolStripMenuItem_Click);
             // 
@@ -104,16 +96,16 @@ namespace CarRentalProject
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsiLogintxt});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 796);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 798);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1495, 24);
+            this.statusStrip1.Size = new System.Drawing.Size(1495, 22);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // tsiLogintxt
             // 
             this.tsiLogintxt.Name = "tsiLogintxt";
-            this.tsiLogintxt.Size = new System.Drawing.Size(0, 18);
+            this.tsiLogintxt.Size = new System.Drawing.Size(0, 16);
             // 
             // MainWindow
             // 
@@ -143,7 +135,6 @@ namespace CarRentalProject
         private System.Windows.Forms.ToolStripMenuItem manageRentedRecordsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem addRentedRecordsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewRecordsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem editbRentedRecordsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem manageUsersToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel tsiLogintxt;
